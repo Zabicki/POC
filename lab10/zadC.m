67b61bde73738f3cceccd5e6c17d69d31b1460d4
Krzysztof <krzysztof.zabicki7@gmail.com>
2019-12-11 11:56:37 +0100

    Fix zadC lab 10

--- lab10/zadC.m
+++ lab10/zadC.m
@@ -1,53 +1,34 @@
+close all;
 clearvars;
 clc;
-close all;
-
-%% images
-
-lab112 = imread('lab112.png');
-dom = imread('dom.png');
-image = dom;
-
-%% Task
-
-figure;
-
-% binarise
-image = im2bw(image, 37/255);
-
-% czyszczenie krawedzi
-image = not(image);
-image = imclearborder(image);
-image = not(image);
-
-% delete vertical lines
-square = strel('square', 5);
-image = imclose(image, square);
-
-imshow(image);
-
-% krawedzie
-edges = edge(image, 'canny');
-
-[H, T, R] = hough(edges);
-
-figure;
-imshow(H, []);
-P = houghpeaks(H, 8);
-hold on;
-plot(P, 'o');
-
-lines = houghlines(edges, T, R, P, 'FillGap', 5, 'MinLength', 7);
-figure, imshow(edges), hold on
+dom = imread("dom.png");
+lab112 = imread("lab112.png");
+img = dom;
+imshow(img);
+figure();
+img = im2bw(img,50/255);
+imshow(img);
+figure();
+SE = strel('square',7);
+img = imclose(img,SE);
+imshow(img);
+img = edge(img,'log',0.30,0.400);
+figure();
+imshow(img)
+[H,T,R] = hough(img);
+peaks = houghpeaks(H,8);
+imgorg = img;
+lines = houghlines(img,T,R,peaks,'FillGap',5,'MinLength',7);
+figure, imshow(imgorg), hold on
 max_len = 0;
 for k = 1:length(lines)
    xy = [lines(k).point1; lines(k).point2];
    plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
-
+ 
    % Plot beginnings and ends of lines
    plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
    plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
-
+ 
    % Determine the endpoints of the longest line segment
    len = norm(lines(k).point1 - lines(k).point2);
    if ( len > max_len)
@@ -55,6 +36,11 @@ for k = 1:length(lines)
       xy_long = xy;
    end
 end
-
-% highlight the longest line segment
-plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','cyan');
+figure();
+img = imread("dom.png");
+imgk = edge(img,'canny',0.4,0.4);
+[H,T,R] = hough(imgk);
+imshow(H,[]);
+hold on;
+peaks = houghpeaks(H,7);
+plot(peaks(:,2),peaks(:,1),'o');
